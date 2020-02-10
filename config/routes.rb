de4a23b093be10b137dab38e355cb61a16fac39f
Stovenn <jonathan.sylvestre.pro@gmail.com>
2020-02-11 00:14:57 +0100

    index fix

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   get 'user/show/:id', to: 'user#show', as:'user'
   get 'gossip/index'
   get 'gossip/show/:id', to: 'gossip#show', as:'gossip'
-  get '/welcome/:first_name', to: 'root#index', as:'index'
+  get '/welcome/', to: 'root#index', as:'index'
+  get '/welcome/:first_name', to: 'root#index', as:'hidden_index'
   get 'contact/show', to: 'contact#show'
   get 'team/show', to: 'team#show'
 end
